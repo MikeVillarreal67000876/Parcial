6d66170939f0e0f76cd3bbebd14260b9b6c80b9f
MikeVillarreal67000876 <Mavillarreal76@ucatolica.edu.co>
2022-10-26 23:04:04 -0500

    Parcial modificado

--- src/main/java/com/mycompany/villarrealmike/VillarrealMike.java
+++ src/main/java/com/mycompany/villarrealmike/VillarrealMike.java
@@ -5,6 +5,9 @@
 
 package com.mycompany.villarrealmike;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  Mike Anderson Villarreal Agudelo
   67000876
@@ -13,6 +16,34 @@ package com.mycompany.villarrealmike;
 public class VillarrealMike {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+  List<Tarea>listaTareas=new ArrayList<Tarea>();
+  Tarea t1=new Tarea();
+  Tarea t2=new Tarea();
+  
+  
+  t1.doEjecutar();
+  t2.doEjecutar();
+  listaTareas.add(t1);
+  listaTareas.add(t2);
+  
+  
+  
+  List<Resultado>listaresultado=new ArrayList<Resultado>();
+  Resultado r1=new Resultado();
+  Resultado r2=new Resultado();
+  
+  r1.ejecutoConError();
+  r1.ejecutoConAdvertencia();
+  r1.ejecutoDi();
+  
+  r2.ejecutoConAdvertencia();
+  r2.ejecutoConError();
+  r2.ejecutoDi();
+  
+  listaresultado.add(r2);
+  listaresultado.add(r1);
+  
+  
+
     }
 }
